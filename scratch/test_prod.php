9f714850b1556713dedeb607fb47455c70e5394a
Admin <aaa@aaa.com>
2026-08-08 02:43:13 +0300

    Add temporary test script

---
+++ scratch/test_prod.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+$app = require_once __DIR__ . '/bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->bootstrap();
+
+$product = Webkul\Product\Models\Product::first();
+if ($product) {
+    $helper = app(Webkul\Product\ProductImage::class);
+    $images = $helper->getGalleryImages($product);
+    echo "GALLERY IMAGES RESULT:\n";
+    print_r($images);
+} else {
+    echo "No product found.\n";
+}
