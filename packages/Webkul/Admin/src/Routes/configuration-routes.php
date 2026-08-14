73dc300a11076731146fded0c85e25369c089413
Admin <aaa@aaa.com>
2026-08-14 23:05:03 +0300

    fix(routes): route download explicitly before index to prevent 404 on configuration index

--- packages/Webkul/Admin/src/Routes/configuration-routes.php
+++ packages/Webkul/Admin/src/Routes/configuration-routes.php
@@ -13,9 +13,9 @@ Route::post('configuration/cache-management/execute', [CacheManagementController
     ->name('admin.configuration.cache-management.execute');
 
 Route::controller(ConfigurationController::class)->group(function () {
+    Route::get('configuration/{slug}/{slug2}/{path}', 'download')->name('admin.configuration.download');
+
     Route::get('configuration/{slug?}/{slug2?}', 'index')->name('admin.configuration.index');
 
     Route::post('configuration/{slug?}/{slug2?}', 'store')->name('admin.configuration.store');
-
-    Route::get('configuration/{slug}/{slug2}/{path}', 'download')->name('admin.configuration.download');
 });
