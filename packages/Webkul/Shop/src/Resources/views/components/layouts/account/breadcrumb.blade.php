be6838b6ef96fe5e8f6b3ac689a1c75b0686978a
Admin <aaa@aaa.com>
2026-08-15 04:15:08 +0300

    Remove breadcrumbs from customer account layout

--- packages/Webkul/Shop/src/Resources/views/components/layouts/account/breadcrumb.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/account/breadcrumb.blade.php
@@ -1,5 +1,2 @@
-<div class="mt-8 flex justify-start max-lg:hidden">
-    <div class="flex items-center gap-x-3.5">
-        @yield('breadcrumbs')
-    </div>
-</div>
+{{-- Customer Account Breadcrumbs (Disabled) --}}
+
