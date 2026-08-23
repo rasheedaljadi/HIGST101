c517da3d22e6dac2b872993ec2a2948b4d183f63
Admin <aaa@aaa.com>
2026-08-23 04:31:20 +0300

    fix(tests): use fully qualified Console Kernel contract in isolated test runner

--- scripts/run_address_guard_tests_isolated.php
+++ scripts/run_address_guard_tests_isolated.php
@@ -3,7 +3,7 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-$kernel = $app->make(Kernel::class);
+$kernel = $app->make(\Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
 use App\Models\AliExpressToken;
